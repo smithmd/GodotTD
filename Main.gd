4ba5ab1359347b20516d71932664e44ca7cac155
Michael Smith <mike@noiseparticles.com>
2018-11-09 17:37:52 -0500

    stuff is walking, but I'm not sure what path they're following....

--- Main.gd
+++ Main.gd
@@ -2,20 +2,20 @@ extends Node2D
 
 export (PackedScene) var Enemy
 var enemies = []
-var paths = []
 
 func _ready():
-	paths = [$PathNorth/PathFollow, $PathSouth/PathFollow]
 	randomize()
 	spawn_enemy()
 	$Spawn.start()
 
 func _on_Enemy_Walk_timeout():
-	for path in paths:
+	#for path in paths:
 	#	print("%: %", path, path.get_unit_offset())
-		path.set_unit_offset(path.get_unit_offset() + .01)
-		for enemy in enemies:
-			enemy.position = path.global_position
+	#	path.set_unit_offset(path.get_unit_offset() + .01)
+		
+	for enemy in enemies:
+		enemy.get_parent().set_unit_offset(enemy.get_parent().get_unit_offset() + .001)
+		enemy.position = enemy.get_parent().global_position
 
 func _on_Spawn_timeout():
 	print("enemy count: ", enemies.size())
@@ -23,8 +23,18 @@ func _on_Spawn_timeout():
 	#pass
 
 func spawn_enemy():
+	var paths = [$PathNorth, $PathSouth]
 	var path = paths[randi() % paths.size()]
+	
+	# create a new PathFollow2D
+	var pf = PathFollow2D.new()
+	path.add_child(pf)
+	pf.set_name("PathFollow")
+	print(path.get_children())
+	
+	# create a new Enemy and add it to the PathFollow2D
 	var enemy = Enemy.instance()
-	enemy.set_path_follow(path)
+	enemy.set_path_follow(pf)
 	enemies.append(enemy)
-	add_child(enemy)
+	pf.add_child(enemy)
+	enemy.position = pf.global_position
